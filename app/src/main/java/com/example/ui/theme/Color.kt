1067e23e1434f503a838808e169b962d6e044723
milkbarandcoffe-web <milkbarandcoffe@gmail.com>
2026-06-29 13:13:35 +0200

    deploy v7: app/src/main/java/com/example/ui/theme/Color.kt

---
+++ app/src/main/java/com/example/ui/theme/Color.kt
@@ -0,0 +1,18 @@
+package com.example.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Elegant Dark Color Scheme
+val ElegantDarkBg = Color(0xFF1A1C1E)
+val ElegantDarkSurface = Color(0xFF222426)
+val ElegantDarkViewport = Color(0xFF000000)
+val ElegantDarkTextPrimary = Color(0xFFE2E2E6)
+val ElegantDarkTextSecondary = Color(0xFFA2A6AE)
+
+// Vibrant Accent Colors (Blues and Teals)
+val ElegantBluePrimary = Color(0xFFD1E1FF)
+val ElegantBlueDarkText = Color(0xFF002F66)
+val ElegantBlueActive = Color(0xFF78D1FF)
+val ElegantAccentPink = Color(0xFFE91E63)
+val ElegantSurfaceVariant = Color(0xFF2E3133)
+val ElegantBorderSilver = Color(0xFF3E4246)
